2d08a00af7f6241de928d9d6464e6c5ab9d07cf6
GraiseZhou <graisez@seas.upenn.edu>
2023-05-07 14:37:40 -0400

    add testing for databasemanage class method createauthormap

---
+++ 5940_Project/src/DataBaseManageTest.java
@@ -0,0 +1,11 @@
+package databaseFramework;
+
+import org.junit.Test;
+
+public class DataBaseManageTest {
+
+    @Test
+    public void testCreateAuthorMap() {
+        List<Poem> poems =CSVFileReader.readCSVFile("poem_data.csv");
+        HashMap<String, List<Poem>> map=DataBaseManage.createAuthorMap(poems);
+        assertTrue(map.size()>0)
